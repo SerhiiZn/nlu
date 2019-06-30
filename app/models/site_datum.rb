d31baf08fa677404106d095782df0aa154c2a953
Vladyslav Sydorenko <svuitis@gmail.com>
2019-06-30 15:14:54 +0300

    show all results

--- app/models/site_datum.rb
+++ app/models/site_datum.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 # Contains url and parsed data from site
 class SiteDatum < ApplicationRecord
-  has_many :analizes
+  has_many :analizes, foreign_key: 'site_data_id'
 
   def self.valid_url?(url)
     !URI.parse(url).host.nil?
